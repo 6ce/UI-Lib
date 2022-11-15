bc4368fd338047d6fe1a17abf61072fcbe910c1d
magnet <86327263+6ce@users.noreply.github.com>
2022-11-14 21:27:50 -0800

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,22 @@
+local Library;
+
+local Success, Error = pcall(function()
+    local Response = syn.request({
+        Url = "https://raw.githubusercontent.com/6ce/UI-Lib/main/Main.lua",
+        Method = "GET"
+    })
+    
+    Library = loadstring(Response.Body)
+end)
+
+if Error then
+    return warn("MagHub error: " .. Error)
+end
+
+Library:MakeNotification({
+    Name = "MAGNET",
+    Contet = "m_gnet was here",
+    Time = 3
+})
+
+return Library
